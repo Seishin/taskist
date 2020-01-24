6c5c85d2b63caa093c665c71a9f13602bfd0ce0c
Atanas Dimitrov <seishin90@yandex.ru>
2020-01-24 10:56:39 +0200

    Adds Firebase Analytics.

--- app/src/main/java/me/seishin/taskist/di/AppModule.kt
+++ app/src/main/java/me/seishin/taskist/di/AppModule.kt
@@ -1,5 +1,6 @@
 package me.seishin.taskist.di
 
+import com.google.firebase.analytics.FirebaseAnalytics
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import me.seishin.taskist.data.AppDatabase
@@ -10,5 +11,6 @@ import org.koin.dsl.module
 
 val appModule = module {
     single { AppDatabase(androidContext()).get() }
+    single { FirebaseAnalytics.getInstance(get()) }
     single<TasksRepository> { TasksRepositoryImpl(get(), Schedulers.io(), AndroidSchedulers.mainThread()) }
 }
